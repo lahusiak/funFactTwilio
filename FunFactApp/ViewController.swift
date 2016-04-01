fd032f5a5c4b47946b3a93445d22e4d93d755708
lahusiak <lahusiak@gmail.com>
2016-04-01 10:25:39 -0500

    Connected to number api

--- FunFactApp/ViewController.swift
+++ FunFactApp/ViewController.swift
@@ -16,22 +16,96 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var funFactLabel: UILabel!
     
+    
     override func viewDidLoad() {
         funFactLabel.text = "I'm a fun fact"
+       
         super.viewDidLoad()
+        retrieveFunFact()
+    
+        
 
         
     }
-        
+    override func viewDidLayoutSubviews(){
+        funFactLabel.numberOfLines = 3
+        funFactLabel.sizeToFit()
+    }
 
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func retrieveFunFact(){
+        let postEndpoint: String = "http://numbersapi.com/random/year?json"
+        
+        
+        //guard should check to see if url is valid
+        guard let url = NSURL(string: postEndpoint)else{
+            print("Error: cannot create url")
+            return
+        }
+        
+        //    let url = NSURL(string: postEndpoint)
+        
+        let urlRequest = NSURLRequest(URL: url)
+        
+        //Create a session to send request
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        
+        //implement tasks for completion handler
+        //what will be done with the response from API
+        let task = session.dataTaskWithRequest(urlRequest, completionHandler: {( data, response, error) in
+            
+            //Handle error
+            guard let responseData = data else{
+                print("Error: did not receive data")
+                return
+            }
+            
+            guard error == nil else{
+                print("error calling GET on /posts/1")
+                print(error)
+                return
+            }
+            
+            //parse the result as JSON, since that's what the API returns
+            let post: NSDictionary
+            
+            do{
+                post = try NSJSONSerialization.JSONObjectWithData(responseData, options: []) as! NSDictionary
+            }catch{
+                print("error trying to convert data to JSON")
+                return
+            }
+            print("The fact is : " + post.description)
+            
+            //the post object is a dictionary
+            //access title with 'title key
+            //check for a title and print it if we have one
+            
+            if let postTitle = post["text"] as? String {
+                print("The title is : " + postTitle)
+                
+                dispatch_async(dispatch_get_main_queue()){
+                   
+                    self.funFactLabel.text = postTitle
+                    
+                }
+            }
+        })
+        
+        task.resume()
+    }
+
 
     @IBAction func showFunFact() {
-        funFactLabel.text = "Check me out!"
+//        funFactLabel.text = "Another fact"
+        retrieveFunFact()
        
     }
 
---
+++ FunFactApp/funFactStruct.swift
@@ -0,0 +1,19 @@
+//
+//  funFactStruct.swift
+//  FunFactApp
+//
+//  Created by Laryssa Husiak on 4/1/16.
+//  Copyright © 2016 Laryssa Husiak. All rights reserved.
+//
+
+import Foundation
+
+struct funFact {
+    let title: String
+    let number: Int
+    let date: String
+    let year: String
+    
+    
+   
+}
