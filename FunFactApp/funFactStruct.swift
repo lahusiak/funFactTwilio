d3f93ba7863c190cc4e7b08562d2587cbb16c9d8
lahusiak <lahusiak@gmail.com>
2016-04-01 17:44:35 -0500

    Sending to github

--- FunFactApp/funFactStruct.swift
+++ FunFactApp/funFactStruct.swift
@@ -1,5 +1,5 @@
 //
-//  funFactStruct.swift
+//  FunFactStruct.swift
 //  FunFactApp
 //
 //  Created by Laryssa Husiak on 4/1/16.
@@ -8,12 +8,20 @@
 
 import Foundation
 
-struct funFact {
-    let title: String
-    let number: Int
-    let date: String
-    let year: String
+struct FunFact {
+    let fact: String?
+    let number: Int?
+    let date: String?
+    let year: String?
+    
+    init(factDictionary: [String: AnyObject]){
+        
+        fact = factDictionary["text"] as? String
+        number = factDictionary["number"] as? Int
+        date = factDictionary["date"] as? String
+        year = factDictionary["year"] as? String
+        
+    }
     
     
-   
 }
